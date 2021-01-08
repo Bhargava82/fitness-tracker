56f2fb184d10aa1a9be9cd61582951fae4695b2e
Bhargava Patel <bhargavapatel82@gmail.com>
2021-01-07 22:04:07 -0500

    corrected mistakes.

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-// const db = require("./models");
+const db = require("./models");
 
 const app = express();
 
-// app.use(logger("dev"));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
